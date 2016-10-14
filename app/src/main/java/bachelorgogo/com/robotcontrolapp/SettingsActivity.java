0fb41b29ee3f6d13d7cc87d148c5a9590ec862db
THP <mikku_pedersen@hotmail.com>
2016-10-14 10:34:54 +0200

    Updated settingsactivity to new architecture.

--- app/src/main/java/bachelorgogo/com/robotcontrolapp/SettingsActivity.java
+++ app/src/main/java/bachelorgogo/com/robotcontrolapp/SettingsActivity.java
@@ -1,22 +1,31 @@
 package bachelorgogo.com.robotcontrolapp;
 
+import android.content.ComponentName;
 import android.content.Context;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.IBinder;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.support.v7.app.ActionBar;
-import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 // Adapted from the template Settings Activity and Android doc @ https://developer.android.com/reference/android/preference/PreferenceActivity.html
-public class SettingsActivity extends AppCompatPreferenceActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final String TAG = "SettingsActivity";
+
+    // WifiService stuff
+    static WiFiDirectService mService;
+    static boolean mBound;
+    static boolean mUnsyncedChanges;
+
     /**
      * A preference value change listener that updates the preference's summary
      * to reflect its new value.
@@ -74,6 +83,14 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferenceFragment()).commit();
         setupActionBar();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        mUnsyncedChanges = false;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.d(TAG,"Preferences changed");
+        mUnsyncedChanges = true;
     }
 
     private void setupActionBar() {
@@ -85,8 +102,64 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        unbindFromService();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d("onServiceConnected", "called");
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            WiFiDirectService.LocalBinder binder = (WiFiDirectService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    protected void bindToService(Intent service) {
+        Log.d("bindToService", "called");
+        bindService(service, mConnection, 0/*Context.BIND_AUTO_CREATE*/);
+    }
+
+    protected void unbindFromService() {
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+        }
+    }
+
     public static class PreferenceFragment extends android.preference.PreferenceFragment
     {
+        private SettingsObject mSettings;
+        private SharedPreferences mSharedPrefs;
+        private Preference upload_btn;
+
+        private String mDeviceName;
+        private String mVideoQualityIndex;
+        private boolean mPowerSaveMode;
+        private boolean mAssistedDrivingMode;
+
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
@@ -94,6 +167,62 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             addPreferencesFromResource(R.xml.preference_screen);
             bindPreferenceSummaryToValue(findPreference("device_name_preference"));
             bindPreferenceSummaryToValue(findPreference("video_preference"));
+
+            mUnsyncedChanges = false;
+
+            mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            // Create restore point
+            mDeviceName = mSharedPrefs.getString(getString(R.string.settings_device_name_key),getString(R.string.robotName));
+            mVideoQualityIndex = mSharedPrefs.getString(getString(R.string.settings_video_key),"1");
+            mPowerSaveMode = mSharedPrefs.getBoolean(getString(R.string.settings_power_save_mode_key),false);
+            mAssistedDrivingMode = mSharedPrefs.getBoolean(getString(R.string.settings_assisted_driving_mode_key),false);
+
+            mSettings = new SettingsObject() {
+                @Override
+                public void onSuccess() {
+                    mUnsyncedChanges = false;
+                    upload_btn.setEnabled(true);
+                    upload_btn.setSummary(getString(R.string.settings_synced));
+                }
+
+                @Override
+                public void onFailure() {
+                    mUnsyncedChanges = true;
+                    Toast.makeText(getActivity(), getString(R.string.settings_sync_error_toast), Toast.LENGTH_LONG).show();
+                    upload_btn.setEnabled(true);
+                    upload_btn.setSummary(getString(R.string.settings_synced));
+                }
+            };
+
+            upload_btn = (Preference)findPreference(getString(R.string.settings_upload_btn_key));
+            upload_btn.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    upload_btn.setEnabled(false);
+                    mSettings.setSettings(mSharedPrefs.getString(getString(R.string.settings_device_name_key),getString(R.string.robotName)),
+                            mSharedPrefs.getString(getString(R.string.settings_video_key),"1"),
+                            mSharedPrefs.getBoolean(getString(R.string.settings_power_save_mode_key),false),
+                            mSharedPrefs.getBoolean(getString(R.string.settings_assisted_driving_mode_key),false));
+
+                    //mService.sendControlCommand(mSettings);
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public void onDestroy() {
+            //Restore settings if not synced when settings activity is closed
+            if (mUnsyncedChanges) {
+                Log.d(TAG,"Restoring preferences");
+                SharedPreferences.Editor editor = mSharedPrefs.edit();
+                editor.putString(getString(R.string.settings_device_name_key), mDeviceName);
+                editor.putString(getString(R.string.settings_video_key), mVideoQualityIndex);
+                editor.putBoolean(getString(R.string.settings_power_save_mode_key), mPowerSaveMode);
+                editor.putBoolean(getString(R.string.settings_assisted_driving_mode_key), mAssistedDrivingMode);
+                editor.apply();
+            }
+            super.onDestroy();
         }
     }
 }
